4f2c36cee11e4e684d37c68e4b5e40a3ce40636f
Bianca <biancalbarbosa@hotmail.com>
2024-01-19 14:02:48 -0300

    Fix Word Count solution for handling properties that exist on Object’s prototype

--- Exercism/word-count.ts
+++ Exercism/word-count.ts
@@ -7,12 +7,21 @@ export function countWords(subtitle: string) {
     let  lowerWords = words?.map(w => { return w.toLowerCase()})
     
     const result = lowerWords?.reduce<ReduceReturnType>(
-        (accumulator, current) => {
-          accumulator[current] = accumulator[current] || 0;
+        (accumulator, current) => {          
+          accumulator[current] = setValue(accumulator[current]);
+
           return { ...accumulator, [current]: accumulator[current]+= 1 };
         },
         {}, 
       );
 
     console.log(new Map(Object.entries({...result})));
+}
+
+function setValue(value: number | undefined): number {
+    value = value || 0;
+    if( value.toString().indexOf('[native code]') > -1 ) {
+        value = 0;
+    }
+    return value;
 }
